36ade51a6a394f09901ad435fa581cfdb33f9695
merve-meryem <meryemkoyuncu@icloud.com>
2026-01-11 15:33:25 +0300

    fix(NavLink): correct font size for responsive design on NavLink component

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -8,7 +8,7 @@ function Navlink({ link }) {
   return (
     <Link
       to={link.url}
-      className={`rounded p-1 text-[18px] sm:text-xl ${
+      className={`rounded p-1 text-[18px] sm:text-2xl ${
         isActive
           ? "bg-lime-400 text-yellow-600"
           : "text-lime-400 hover:text-yellow-500"

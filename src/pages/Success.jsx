253ef29d90fe3eb6bd3456a2a66c86882d8336db
merve-meryem <meryemkoyuncu@icloud.com>
2026-01-11 17:31:27 +0300

    fix(Success): improve styling and formatting for consistency

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,13 +1,10 @@
-
-import React from 'react'
+import React from "react";
 const Success = () => {
- 
-
   return (
-    <div className="text-3xl text-lime-500 p-50 text-center">
+    <div className="text-xs sm:text-3xl text-lime-500 w-full  text-center items-center pt-10 sm:pt-50">
       ✅ Siparişiniz başarıyla alındı!
     </div>
-  )
-}
+  );
+};
 
-export default Success
+export default Success;
